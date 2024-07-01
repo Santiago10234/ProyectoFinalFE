5f3ec869b66e11b2fd8bc227e10b21bed5386aaf
santiago10234 <ariza.ps4@gmail.com>
2024-07-01 08:21:59 -0600

    primer commit en la branch cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './Pages/App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Inicio from './Pages/Inicio.jsx'
